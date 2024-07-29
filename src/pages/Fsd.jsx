f329e80de4bf1c38c26e279b28ba025c9c432a4d
ShakthiSundar-K <shakthisundar01@gmail.com>
2024-07-30 00:44:02 +0530

    img change

--- src/pages/Fsd.jsx
+++ src/pages/Fsd.jsx
@@ -6,17 +6,17 @@ function Fsd() {
     {
       title: "Best Full-Stack Development Project Ideas in 2024",
       text: "By Tushar Vinocha",
-      img: "https://www.guvi.in/blog/wp-content/uploads/2023/07/person-front-computer-working-html.jpg",
+      img: "https://cors-anywhere.herokuapp.com/https://www.guvi.in/blog/wp-content/uploads/2023/07/person-front-computer-working-html.jpg",
     },
     {
       title: "How Long Would It Take to Be a Full Stack Developer?",
       text: "By Meghana D",
-      img: "https://www.guvi.in/blog/wp-content/uploads/2023/07/how-long-it-would-it-take-to-learn-full-stack-development_-2048x1072.webp",
+      img: "https://cors-anywhere.herokuapp.com/https://www.guvi.in/blog/wp-content/uploads/2023/07/how-long-it-would-it-take-to-learn-full-stack-development_-2048x1072.webp",
     },
     {
       title: "Top 10 Full-Stack Developer Frameworks in 2024",
       text: "By Isha Sharma",
-      img: "https://www.guvi.in/blog/wp-content/uploads/2023/07/html-css-collage-concept-with-person.jpg",
+      img: "https://cors-anywhere.herokuapp.com/https://www.guvi.in/blog/wp-content/uploads/2023/07/html-css-collage-concept-with-person.jpg",
     },
     {
       title: "Best Full-Stack Development Project Ideas in 2024",
